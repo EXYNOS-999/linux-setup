23f236526c871c14dff22396ee590f3045136c24
RISHABH <55334249+EXYNOS-999@users.noreply.github.com>
2020-09-28 11:55:07 +0530

    Update dual_scaling_DP-1_eDP-1.sh

--- dual_scaling_DP-1_eDP-1.sh
+++ dual_scaling_DP-1_eDP-1.sh
@@ -1,3 +1,9 @@
 xrandr --output DP-1 --scale 2x2 --mode 1366x768 --fb 3200x1800 --pos 0x0
 xrandr --output eDP-1 --scale 1x1 --pos 3840x0
 xrandr --output eDP-1 --scale 0.9999x0.9999
+
+
+## manjaro 
+
+xrandr --output eDP1 --auto --output DP1 --auto --scale 1.999x2 --right-of eDP1
+ 
